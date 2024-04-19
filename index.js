f9e0e30f08cc73fd7622f04e2418b8f7af0e629e
Mehwish mazari <mashmazari@gmail.com>
2024-04-19 21:58:33 +0500

    this is a word counter

---
+++ index.js
@@ -0,0 +1,14 @@
+// import the 'inquirer' module, which is a command line interface for Node.js
+import inquirer from "inquirer";
+import chalk from "chalk";
+//Declare a constant 'answers' and assign it to the result of inquirer.prompt function
+const answers = await inquirer.prompt([
+    {
+        name: "sentence",
+        type: "input",
+        message: chalk.red.bold("Enter your sentence to count the word"),
+    },
+]);
+const words = answers.sentence.trim().split("");
+console.log(words);
+console.log(chalk.green.bold(`your sentence to count the word is ${words.length}`));
